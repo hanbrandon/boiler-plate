6589dfce893e308c20b7ea930ef96a89deae4a74
hanbrandon <hanbrandon@hotmail.com>
2020-07-05 17:27:15 -0700

    jsonwebtoken

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
 const { User } = require('./models/User');
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 //application/x-www-form-urlencoded
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose
@@ -25,7 +26,7 @@ mongoose
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 	//회원 가입 할때 필요한 정보들을 Client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
 	const user = new User(req.body);
 	user.save((err, userInfo) => {
@@ -40,4 +41,33 @@ app.post('/register', (req, res) => {
 	});
 });
 
+app.post('/api/users/login', (req, res) => {
+	//요청된 이메일을 데이터베이스에서 찾음.
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (!user) {
+			return res.json({
+				loginSuccess: false,
+				message: 'No account found.'
+			});
+		}
+		//요청된 이메일이 데이터 베이스에 있다면 비밀번호가 동일한지 확인.
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if (!isMatch)
+				return res.json({
+					loginSuccess: false,
+					message: 'Wrong Password'
+				});
+			//비밀번호가 동일하면 토큰 생성하기
+			user.generateToken((err, user) => {
+				if (err) return res.status(400).send(err);
+
+				//토큰을 저장한다. 어디에? 쿠키, 로컬스토리지, 세션
+				res.cookie('x_auth', user.token).status(200).json({
+					loginSuccess: true,
+					userId: user._id
+				});
+			});
+		});
+	});
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
