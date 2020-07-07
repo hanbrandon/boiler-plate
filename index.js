dbcd8c33dbab0eb0dab01833e873c931e30296bd
hanbrandon <hanbrandon@hotmail.com>
2020-07-06 17:04:00 -0700

    logout

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
 const { User } = require('./models/User');
+const { auth } = require('./middleware/auth');
+
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
@@ -61,7 +63,7 @@ app.post('/api/users/login', (req, res) => {
 			user.generateToken((err, user) => {
 				if (err) return res.status(400).send(err);
 
-				//토큰을 저장한다. 어디에? 쿠키, 로컬스토리지, 세션
+				//토큰을 저장한다. 어디에? 쿠키<, 로컬스토리지, 세션
 				res.cookie('x_auth', user.token).status(200).json({
 					loginSuccess: true,
 					userId: user._id
@@ -69,5 +71,36 @@ app.post('/api/users/login', (req, res) => {
 			});
 		});
 	});
+});
+
+app.get('/api/users/auth', auth, (req, res) => {
+	//여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True 라는 말.
+	res.status(200).json({
+		_id: req.user._id,
+		isAdmin: req.user.role === 0 ? false : true,
+		isAuth: true,
+		email: req.user.email,
+		name: req.user.name,
+		lastname: req.user.lastname,
+		role: req.user.role,
+		iamge: req.user.image
+	});
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+	User.findOneAndUpdate(
+		{ _id: req.user._id },
+		{
+			token: ''
+		},
+		(err, user) => {
+			{
+				if (err) return res.json({ success: false, err });
+				return res.status(200).send({
+					success: true
+				});
+			}
+		}
+	);
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
