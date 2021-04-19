b9bedd0ff665d378d7745e4f18869b649d10be04
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-04-19 11:38:34 -0700

    sass implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,31 @@
-import React from 'react';
-import './styles/App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomePage from './pages/Home';
-import LoginPage from './pages/Login';
-import RegisterPage from './pages/Register';
-import ForgotPasswordPage from './pages/ForgotPassword';
-import ResetPasswordPage from './pages/ResetPassword';
-import Footer from './components/global/Footer';
+import React from "react";
+import "./styles/App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+import ForgotPasswordPage from "./pages/ForgotPassword";
+import ResetPasswordPage from "./pages/ResetPassword";
+import Footer from "./components/global/Footer";
 
-import Auth from './hoc/auth';
+import Auth from "./hoc/auth";
 
 const App = () => {
-	return (
-		<>
-			<Switch>
-				<Route exact path="/" component={Auth(HomePage, null)} />
-				<Route exact path="/login" component={Auth(LoginPage, false)} />
-				<Route
-					exact
-					path="/register"
-					component={Auth(RegisterPage, false)}
-				/>
-				<Route
-					exact
-					path="/forgotpassword"
-					component={Auth(ForgotPasswordPage, false)}
-				/>
-				<Route
-					path="/reset/:id"
-					component={Auth(ResetPasswordPage, false)}
-				/>
-			</Switch>
-			<Footer></Footer>
-		</>
-	);
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Auth(HomePage, null)} />
+        <Route exact path="/login" component={Auth(LoginPage, false)} />
+        <Route exact path="/register" component={Auth(RegisterPage, false)} />
+        <Route
+          exact
+          path="/forgotpassword"
+          component={Auth(ForgotPasswordPage, false)}
+        />
+        <Route path="/reset/:id" component={Auth(ResetPasswordPage, false)} />
+      </Switch>
+      <Footer></Footer>
+    </>
+  );
 };
 export default App;
